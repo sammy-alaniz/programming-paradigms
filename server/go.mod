3e2e020c0799cdfe0c0338d6a1ef7437cfdf6a34
sammy <samalaniz25@gmail.com>
2023-09-17 16:11:25 -0500

    go server connected to neo4j

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module server
 
 go 1.19
+
+require github.com/neo4j/neo4j-go-driver/v4 v4.4.7 // indirect
--- server/server.go
+++ server/server.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	// "net/http"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, World!")
+	// })
 
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
+
+	var driver neo4j.Driver
+
+	var address = "neo4j://localhost:7687"
+	var username = "neo4j"
+	var password = "test2023test"
+
+	driver, err := neo4j.NewDriver(address, neo4j.BasicAuth(username, password, ""))
+
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Driver established!")
+
+	defer driver.Close()
 }
