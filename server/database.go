6ada4d0c09fbac091735ab8b54171c67f895b043
sammy <samalaniz25@gmail.com>
2023-09-24 21:07:18 -0500

    This compiles, added function that sHoUld convert a neo4j db query return into a JSON object

--- server/database.go
+++ server/database.go
@@ -12,12 +12,13 @@ type ConnectionData struct {
 	username string
 	password string
 	driver   neo4j.Driver
+	session  neo4j.Session
 	err      error
 	uuid     string
 }
 
 func (cd *ConnectionData) init() {
-	fmt.Println("initalizing neo4j connection")
+	fmt.Println("initalizing connection data")
 
 	cd.uuid = uuid.New().String()
 	fmt.Println(cd.uuid)
@@ -29,12 +30,50 @@ func (cd *ConnectionData) init() {
 	}
 
 	fmt.Println("Driver established!")
+
+	cd.session = cd.driver.NewSession(neo4j.SessionConfig{})
+
+	fmt.Println("Session established!")
 }
 
-// in work func
-// func (cd *ConnectionData) query(query string) (hash string, results map[string]interface{}, err error) {
+func (cd *ConnectionData) query(query string) (hash string, results map[string]interface{}, err error) {
 
-// }
+	rs, er := cd.session.Run(query, nil)
+
+	if er != nil {
+		return cd.uuid, nil, er
+	}
+
+	results = cd.neo4j_to_http(rs)
+
+	return cd.uuid, results, er
+}
+
+// this runs but is untested
+func (cd *ConnectionData) neo4j_to_http(data neo4j.Result) (rtn_data map[string]interface{}) {
+
+	rtn_data = make(map[string]interface{})
+
+	for data.Next() {
+		record := data.Record()
+
+		for _, key := range record.Keys {
+			value, valid := record.Get(key)
+
+			if !valid {
+				// maybe do more error handling?
+				continue
+			}
+
+			rtn_data[key] = value
+
+			fmt.Printf("Key: %s, Value: %v\n", key, value)
+		}
+
+	}
+
+	return rtn_data
+}
 
 func (cd *ConnectionData) shutdown() {
 	if cd.driver == nil {
