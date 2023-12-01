defa7fffc8f4e558ceb9fd123cf952dc55d59825
sammy <samalaniz25@gmail.com>
2023-12-01 16:42:55 -0600

    code in ocaml works!

--- ocaml/exam/bin/main.ml
+++ ocaml/exam/bin/main.ml
@@ -1,1 +1,14 @@
 let () = print_endline "Hello, World!"
+
+let exp_list lst =
+  let rec aux lst index =
+    match lst with
+    | [] -> []
+    | hd :: tl -> (int_of_float (float_of_int hd ** float_of_int (index + 1))) :: aux tl (index + 1)
+  in
+  aux lst 0
+
+  let () =
+  let result = exp_list [1; 2; 6] in
+  List.iter (Printf.printf "%d; ") result;
+  Printf.printf "\n"
